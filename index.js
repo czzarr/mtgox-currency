8be61ed24b359e875cd23e438752f8292f8ab93d
Stanislas Marion <stanislas.marion@gmail.com>
2014-02-18 01:39:17 +0100

    use bignum

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var bignum = require('bignum')
+
 var btcMultiple = 100000000 //1e8
 var btcSuffix = '\u00a0BTC'
 var usdCurrency = 'USD'
@@ -7,43 +9,43 @@ var usdPrefix = '$'
 var currency = {};
 
 // converts mtgox bitcoin integer in float string
-currency.btcInt2FloatString = function (value_int) {
-  return (value_int / btcMultiple).toFixed(8);
+currency.btcBignum2FloatString = function (value_bignum) {
+  return (value_bignum.toNumber() / btcMultiple).toFixed(8);
 }
 
 // converts mtgox bitcoin integer in display string
-currency.btcInt2Display = function (value_int) {
-  return (value_int / btcMultiple).toFixed(8) + btcSuffix;
+currency.btcBignum2Display = function (value_bignum) {
+  return (value_bignum.toNumber() / btcMultiple).toFixed(8) + btcSuffix;
 }
 
 // converts mtgox bitcoin integer in short display string
-currency.btcInt2DisplayShort = function (value_int) {
-  return (value_int / btcMultiple).toFixed(2) + btcSuffix;
+currency.btcBignum2DisplayShort = function (value_bignum) {
+  return (value_bignum.toNumber() / btcMultiple).toFixed(2) + btcSuffix;
 }
 
 // converts mtgox usd integer in float string
-currency.usdInt2FloatString = function (value_int) {
-  return (value_int / usdMultiple).toFixed(5);
+currency.usdBignum2FloatString = function (value_bignum) {
+  return (value_bignum.toNumber() / usdMultiple).toFixed(5);
 }
 
 // converts mtgox usd integer in display string
-currency.usdInt2Display = function (value_int) {
-  return usdPrefix + (value_int / usdMultiple).toFixed(5);
+currency.usdBignum2Display = function (value_bignum) {
+  return usdPrefix + (value_bignum.toNumber() / usdMultiple).toFixed(5);
 }
 
 // converts mtgox usd integer in short display string
-currency.usdInt2DisplayShort = function (value_int) {
-  return usdPrefix + (value_int / usdMultiple).toFixed(2);
+currency.usdBignum2DisplayShort = function (value_bignum) {
+  return usdPrefix + (value_bignum.toNumber() / usdMultiple).toFixed(2);
 }
 
 // converts user friendly USD amount to MTGOX friendly USD amount
-currency.usdFloat2UsdInt = function (value_float) {
-  return parseInt(value_float * usdMultiple, 10);
+currency.usdFloat2UsdBignum = function (value_float) {
+  return bignum(value_float * usdMultiple);
 }
 
 // converts user friendly USD amount to MTGOX friendly USD amount
-currency.btcFloat2btcInt = function (value_float) {
-  return parseInt(value_float * btcMultiple, 10);
+currency.btcFloat2btcBignum = function (value_float) {
+  return bignum(value_float * btcMultiple);
 }
 
 module.exports = currency;
--- test/index.test.js
+++ test/index.test.js
@@ -1,40 +1,41 @@
+var bignum = require('bignum')
 var should = require('chai').should()
   , currency = require('../')
-  , btcInt = 100000000
+  , btcBignum = bignum(100000000)
   , btcFloatString = '1.00000000'
   , btcDisplay = '1.00000000\u00a0BTC'
   , btcDisplayShort = '1.00\u00a0BTC'
-  , usdInt = 100000
+  , usdBignum = bignum(100000)
   , usdFloatString = '1.00000'
   , usdDisplay = '$1.00000'
   , usdDisplayShort = '$1.00'
   ;
 
 describe.only('Test currency utility functions', function () {
-  it('Convert a BTC Gox Integer to a Float String', function () {
-    currency.btcInt2FloatString(btcInt).should.equal(btcFloatString);
+  it('Convert a BTC Gox Bignum to a Float String', function () {
+    currency.btcBignum2FloatString(btcBignum).should.equal(btcFloatString);
   })
-  it('Convert a BTC Gox Integer to a Display String', function () {
-    currency.btcInt2Display(btcInt).should.equal(btcDisplay);
+  it('Convert a BTC Gox Bignum to a Display String', function () {
+    currency.btcBignum2Display(btcBignum).should.equal(btcDisplay);
   })
-  it('Convert a BTC Gox Integer to a Display Short String', function () {
-    currency.btcInt2DisplayShort(btcInt).should.equal(btcDisplayShort);
+  it('Convert a BTC Gox Bignum to a Display Short String', function () {
+    currency.btcBignum2DisplayShort(btcBignum).should.equal(btcDisplayShort);
   })
-  it('Convert a USD Gox Integer to a Float String', function () {
-    currency.usdInt2FloatString(usdInt).should.equal(usdFloatString);
+  it('Convert a USD Gox Bignum to a Float String', function () {
+    currency.usdBignum2FloatString(usdBignum).should.equal(usdFloatString);
   })
-  it('Convert a USD Gox Integer to a Display String', function () {
-    currency.usdInt2Display(usdInt).should.equal(usdDisplay);
+  it('Convert a USD Gox Bignum to a Display String', function () {
+    currency.usdBignum2Display(usdBignum).should.equal(usdDisplay);
   })
-  it('Convert a USD Gox Integer to a Display Short String', function () {
-    currency.usdInt2DisplayShort(usdInt).should.equal(usdDisplayShort);
+  it('Convert a USD Gox Bignum to a Display Short String', function () {
+    currency.usdBignum2DisplayShort(usdBignum).should.equal(usdDisplayShort);
   })
-  it('Convert a USD Float to a USD Gox Integer', function () {
-    currency.usdFloat2UsdInt(123.45).should.equal(12345000);
-    currency.usdFloat2UsdInt(0.4023).should.equal(40230);
+  it('Convert a USD Float to a USD Gox Bignum', function () {
+    currency.usdFloat2UsdBignum(123.45).eq(bignum(12345000)).should.be.true;
+    currency.usdFloat2UsdBignum(0.4023).eq(bignum(40230)).should.be.true;
   })
-  it('Convert a BTC Float to a BTC Gox Integer', function () {
-    currency.btcFloat2btcInt(123.45).should.equal(12345000000);
-    currency.btcFloat2btcInt(0.01).should.equal(1000000);
+  it('Convert a BTC Float to a BTC Gox Bignum', function () {
+    currency.btcFloat2btcBignum(123.45).eq(bignum(12345000000)).should.be.true;
+    currency.btcFloat2btcBignum(0.01).eq(bignum(1000000)).should.be.true;
   })
 });
